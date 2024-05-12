2e7d4e9db0efc3dd0f610da271eb6131e7169cdf
DavideLigari01 <davide.ligari01@universitadipavia.it>
2024-05-12 11:44:04 +0200

    refactoring utils functions

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -19,37 +19,43 @@ from scipy.stats import spearmanr
 # ----------------- Functions ---------------------------
 
 
-def move_corrupted_files(src_dir, dest_dir):
+def move_corrupted_files(src_dir: str, dest_dir: str) -> tuple:
     """
-    Check for corrupted files in a directory. If a file is corrupted, it is moved to a new directory.
-    It prints the names of corrupted files and the total count of corrupted files found in the directory.
+    Checks for corrupted audio files in the source directory and moves them to a destination directory.
 
     Args:
-        dir (str): The directory path to check for corrupted files.
+        src_dir (str): The path to the source directory containing audio files.
+        dest_dir (str): The path to the destination directory where corrupted files will be moved.
 
     Returns:
-        count (int): The total count of corrupted files found in the directory.
-        filenames (list): The names of corrupted files.
-
-    Prints the names of corrupted files and the total count of corrupted files found in the directory.
+        tuple: A tuple containing the count of corrupted files found and a list of their filenames.
     """
     count = 0
     filenames = []
 
+    # Create the destination directory if it doesn't exist
     os.makedirs(dest_dir, exist_ok=True)
 
+    # Iterate through files in the source directory
     for file in tqdm(os.listdir(src_dir), desc="Checking files"):
         try:
-            _ = torchaudio.load(src_dir + file)
+            # Attempt to load the audio file
+            _ = torchaudio.load(os.path.join(src_dir, file))
         except:
-            # cut the file and paste in a new directory
-            os.system(f"mv {src_dir + file} {dest_dir + file}")
+            # Move the corrupted file to the destination directory
+            os.system(
+                f"mv {os.path.join(src_dir, file)} {os.path.join(dest_dir, file)}"
+            )
 
+            # Print an error message and increment the count of corrupted files
             print(ValueError(f"File {file} is corrupted"))
             count += 1
             filenames.append(file)
 
+    # Print the total count of corrupted files found
     print(f"Files corrupted in {src_dir}:\t{count}\n")
+
+    # Return the count of corrupted files and their filenames
     return count, filenames
 
 
@@ -57,7 +63,9 @@ def move_corrupted_files(src_dir, dest_dir):
 # --------------------------------------------------------------------
 
 
-def slicing(waveform, offset=0, num_frames=None):
+def slicing(
+    waveform: np.ndarray | torch.Tensor, offset: int = 0, num_frames: int | None = None
+) -> np.ndarray:
     """
     Slice the waveform into a specified number of frames starting from a given offset.
 
@@ -79,56 +87,45 @@ def slicing(waveform, offset=0, num_frames=None):
 
 
 def extract_features(
-    dir_path, label, frame_length, sample_rate=44100, n_mfcc=13, melkwargs={}
-):
+    dir_path: str,
+    label: str,
+    frame_length: float,
+    sample_rate: int = 44100,
+    n_mfcc: int = 13,
+    melkwargs: dict = {},
+) -> list:
     """
-    Extract features from audio files. It takes the directory path containing the audio files and returns a list of tensors containing features for each audio file.
-    Each element in the list is a tensor of shape (num_frames, n_mfcc+6+2), where num_frames is the number of frames in the audio file, n_mfcc is the number of MFCC coefficients,
-    6 is the number of additional features (Chroma STFT, RMS, Spectral Centroid, Spectral Bandwidth, Spectral Rolloff, Zero Crossing Rate), and the last two columns are the label and the filename.
-    The features are computed on a 1 sec audio segment at a time. As a consequence an audio file with a length of 10 sec will have 10 tensors in the list. Note that if
-    the audio file is stereo, it will be converted to mono before computing the features.
+    Extracts audio features (MFCC, chroma, RMS, spectral centroid, spectral bandwidth, spectral rolloff, zero-crossing rate)
+    from audio files in the specified directory.
 
     Args:
-    - dir_path (str): Directory path containing the audio files.
-    - sample_rate (int): Desired sample rate for audio processing (default: 44100 Hz).
-    - label (int): The label for the audio files (0 for real, 1 for fake).
-    - n_mfcc (int): Number of MFCC coefficients to compute (default: 13).
-    - melkwargs (dict): Additional arguments for Mel-scale transformation (default: {}).
+        dir_path (str): The path to the directory containing audio files.
+        label (str): The label associated with the extracted features.
+        frame_length: The frame length for feature extraction.
+        sample_rate (int, optional): The sample rate of the audio files. Defaults to 44100 Hz.
+        n_mfcc (int, optional): The number of Mel-frequency cepstral coefficients (MFCCs) to extract. Defaults to 13.
+        melkwargs (dict, optional): Additional arguments for Mel spectrogram computation.
 
     Returns:
-    - features (list): List of tensors containing features for each audio file.
+        list: A list containing tensors of extracted features for each audio file.
     """
-
     filenames = os.listdir(dir_path)
     filenames = [file for file in filenames if not file.startswith(".")]
     frame_length = int(1 / frame_length)
 
-    #     print(f"Checking the sample rate of the audio files in {dir_path}...")
-    #     for file in filenames:
-    #         metadata = torchaudio.info(dir_path + file)
-    #         # Check the sample rate of the audio. If different from the desired sample rate, raise an error
-    #         if metadata.sample_rate != sample_rate:
-    #             raise ValueError(f"Sample rate of the audio {file} is {metadata.sample_rate} Hz. It should be {sample_rate} Hz.\nPlease resample using the provided function 'check_resample_sample_rate'.")
-
     features = []  # List to store the features for all files
 
     for file in tqdm(filenames, desc=f"Extraction in progress"):
-        audio, orig_sample_rate = torchaudio.load(dir_path + file)
+        audio, orig_sample_rate = torchaudio.load(os.path.join(dir_path, file))
         orig_sample_rate_tmp = int(orig_sample_rate / frame_length)
-        audio_length = int(
-            max(audio[0].shape) / (orig_sample_rate_tmp)
-        )  # Length of the audio in half seconds. Discard the remainder.
+        audio_length = int(max(audio[0].shape) / (orig_sample_rate_tmp))
 
         sample_rate = orig_sample_rate
-        # Check the sample rate of the audio. If different from the desired sample rate, raise an error
-        # if orig_sample_rate != sample_rate:
-        #    raise ValueError(f"Sample rate of the audio {file} is {orig_sample_rate} Hz. It should be {sample_rate} Hz.\nPlease resample using the provided function 'check_resample_sample_rate'.")
 
         # Reduce the audio from stereo to mono if needed
         if audio.shape[0] > 1:
             print(f"Converting stereo audio to mono for {file}...")
             audio = torch.mean(audio, dim=0).reshape(1, -1)
-            # print(f"Shape of the mono audio: {audio_mono.shape}")
 
         features_local = []  # List to store the MFCC features for each file
 
@@ -140,21 +137,14 @@ def extract_features(
                 num_frames=int(sample_rate / frame_length * (i + 1)),
             )
 
-            # get the MFCC features
+            # Get the MFCC features
             mfcc_features_tmp = extract_mfcc(audio_mono, sample_rate, n_mfcc, melkwargs)
-            # print(f'MFCC features shape: {mfcc_features_tmp.shape}')
             chroma_stft = extract_chroma_stft(audio_mono, sample_rate)
-            # print(f'Chroma STFT shape: {chroma_stft.shape}')
             rms = extract_rms(audio_mono)
-            # print(f'RMS shape: {rms.shape}')
             spec_cent = extract_spectral_centroid(audio_mono, sample_rate)
-            # print(f'Spectral Centroid shape: {spec_cent.shape}')
             spec_bw = extract_spectral_bandwidth(audio_mono, sample_rate)
-            # print(f'Spectral Bandwidth shape: {spec_bw.shape}')
             rolloff = extract_spectral_rolloff(audio_mono, sample_rate)
-            # print(f'Spectral Rolloff shape: {rolloff.shape}')
             zcr = extract_zero_crossing_rate(audio_mono)
-            # print(f'Zero Crossing Rate shape: {zcr.shape}')
 
             # Concatenate the features
             features_tmp = torch.cat(
@@ -163,19 +153,20 @@ def extract_features(
             )
             features_local.append(features_tmp)
 
-        # if features_local is empty, skip the file
+        # If features_local is empty, skip the file
         if features_local == []:
             print(f"No features extracted for {file}. Skipping...")
             continue
 
         # Stack the MFCC features into a single tensor
         features_local = torch.stack(features_local, dim=0)
-        # attach the label in the last column
 
+        # Attach the label in the last column
         features_local = torch.cat(
             (features_local, torch.ones(features_local.shape[0], 1) * label), dim=1
         )
-        # attach the filename in the last column
+
+        # Attach the filename index in the last column
         features_local = torch.cat(
             (
                 features_local,
@@ -194,7 +185,7 @@ def extract_features(
 # --------------------------------------------------------------------
 
 
-def save_features(data, path, name):
+def save_features(data: torch.Tensor, path: str, name: str) -> None:
     """
     Save the features to a file.
 
@@ -231,7 +222,9 @@ def save_features(data, path, name):
 # --------------------------------------------------------------------
 
 
-def extract_mfcc(audio, sample_rate, n_mfcc, melkwargs):
+def extract_mfcc(
+    audio: torch.Tensor, sample_rate: int, n_mfcc: int, melkwargs: dict
+) -> torch.Tensor:
     """
     Extract MFCC features from audio files. It takes the directory path containing the audio files and returns a list of tensors containing MFCC features for each audio file.
     Each element in the list is a tensor of shape (num_frames, n_mfcc+2), where num_frames is the number of frames in the audio file, n_mfcc is the number of MFCC coefficients,
@@ -261,7 +254,7 @@ def extract_mfcc(audio, sample_rate, n_mfcc, melkwargs):
 # --------------------------------------------------------------------
 
 
-def extract_chroma_stft(audio, sample_rate):
+def extract_chroma_stft(audio: torch.Tensor, sample_rate: int) -> torch.Tensor:
     """
     Extract Chroma STFT features from an audio file.
 
@@ -281,7 +274,7 @@ def extract_chroma_stft(audio, sample_rate):
 # --------------------------------------------------------------------
 
 
-def extract_rms(audio):
+def extract_rms(audio: torch.Tensor) -> torch.Tensor:
     """
     Extract RMS features from an audio file.
 
@@ -300,7 +293,7 @@ def extract_rms(audio):
 # --------------------------------------------------------------------
 
 
-def extract_spectral_centroid(audio, sample_rate):
+def extract_spectral_centroid(audio: torch.Tensor, sample_rate: int) -> torch.Tensor:
     """
     Extract Spectral Centroid features from an audio file.
 
@@ -322,7 +315,7 @@ def extract_spectral_centroid(audio, sample_rate):
 # --------------------------------------------------------------------
 
 
-def extract_spectral_bandwidth(audio, sample_rate):
+def extract_spectral_bandwidth(audio: torch.Tensor, sample_rate: int) -> torch.Tensor:
     """
     Extract Spectral Bandwidth features from an audio file.
 
@@ -344,7 +337,7 @@ def extract_spectral_bandwidth(audio, sample_rate):
 # --------------------------------------------------------------------
 
 
-def extract_spectral_rolloff(audio, sample_rate):
+def extract_spectral_rolloff(audio: torch.Tensor, sample_rate: int) -> torch.Tensor:
     """
     Extract Spectral Rolloff features from an audio file.
 
@@ -364,7 +357,7 @@ def extract_spectral_rolloff(audio, sample_rate):
 # --------------------------------------------------------------------
 
 
-def extract_zero_crossing_rate(audio):
+def extract_zero_crossing_rate(audio: torch.Tensor) -> torch.Tensor:
     """
     Extract Zero Crossing Rate features from an audio file.
 
@@ -384,8 +377,13 @@ def extract_zero_crossing_rate(audio):
 
 
 def generate_samples(
-    dir, target_num, noise_factor, speed_factor, pitch_factor, marker=""
-):
+    dir: str,
+    target_num: int,
+    noise_factor: list,
+    speed_factor: list,
+    pitch_factor: list,
+    marker: str = "",
+) -> None:
     """
     Generate additional audio samples to balance the dataset.
 
@@ -482,7 +480,7 @@ def generate_samples(
 # --------------------------------------------------------------------
 
 
-def remove_generated_samples(dir, marker):
+def remove_generated_samples(dir: str, marker: str) -> None:
     """
     Remove the generated audio samples from the directory.
 
@@ -507,38 +505,49 @@ def remove_generated_samples(dir, marker):
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
 
+import os
+import shutil
 
-def get_move_outliers(src_dir, out_dir, classname, outliers, audio_info, move=False):
+
+def get_move_outliers(
+    src_dir: str,
+    out_dir: str,
+    classname: str,
+    outliers: pd.DataFrame,
+    audio_info: pd.DataFrame,
+    move: bool = False,
+) -> list:
     """
-    Get outliers for a given class and move them to the outliers folder.
+    Retrieves and optionally moves outliers for a given class from the source directory to an output directory.
 
-    Parameters:
-    src_dir (str): The source directory where the audio files are located.
-    classname (str): The name of the class for which outliers are to be retrieved.
-    move (bool, optional): Flag indicating whether to move the outliers to the outliers folder.
-                           Defaults to False.
+    Args:
+        src_dir (str): The path to the source directory containing audio files.
+        out_dir (str): The path to the output directory where outliers will be moved.
+        classname (str): The name of the class for which outliers are to be handled.
+        outliers: Dataframe containing information about outliers.
+        audio_info: Dataframe containing information about all audio files.
+        move (bool, optional): If True, move the outliers to the output directory. Defaults to False.
 
     Returns:
-    out (list): A list of filenames of the outliers for the given class.
+        list: A list of filenames of the outliers for the specified class.
     """
-
+    # Retrieve outliers for the specified class
     out = outliers[outliers["label"] == classname]["filename"].to_list()
     print(f"{classname} outliers {out}")
     print(f"number of {classname} outliers {len(out)}")
 
-    # drop the outliers for {classname}
+    # Drop the outliers for the specified class
     audio_info_outliers = audio_info[~audio_info["filename"].isin(out)]
 
     print("directory size", len(os.listdir(src_dir)))
 
-    if move == False:
+    if not move:
         return out
-    elif move == True:
-
-        # move the outliers to the outliers folder using shutil
-        os.makedirs(out_dir + classname, exist_ok=True)
+    elif move:
+        # Move the outliers to the output directory
+        os.makedirs(os.path.join(out_dir, classname), exist_ok=True)
         for file in out:
-            shutil.move(src_dir + file, out_dir + classname)
+            shutil.move(os.path.join(src_dir, file), os.path.join(out_dir, classname))
 
         print("post-removal directory size", len(os.listdir(src_dir)))
         return out
